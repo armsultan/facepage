9e17a94f81114bab70f9206d8c2565dad565c853
AUpchurch42 <gaffelstein@gmail.com>
2017-05-22 23:00:21 -0400

    registration

--- build/components/Registration.js
+++ build/components/Registration.js
@@ -15,16 +15,19 @@ export default class Registration extends React.Component{
         return (
             <div>
              <h1>Registration</h1>
-               <h3>Sign Up</h3> 
+               
                 <div>
-                    <label>Email: <input type="email" ref="email" /></label>
-                    <label>Password: <input type="password" ref="password" /></label>
-                    <label>First Name: <input type="text" ref="firstname" /></label>
-                    <label>Last Name: <input type="text" ref="lastname" /></label>
-                    <label>Age: <input type="number" ref="age" /></label>
-                    <label>Gender: <input type="radio" ref="gender" /></label>
-                    <label>School: <input type="text" ref="school" /></label>
-                    <label>Job: <input type="text" ref="job" /></label>
+                    <label>Email: <input type="email" ref="email" /></label><br/>
+                    <label>Password: <input type="password" ref="password" /></label><br/>
+                    <label>First Name: <input type="text" ref="firstname" /></label><br/>
+                    <label>Last Name: <input type="text" ref="lastname" /></label><br/>
+                    <label>Age: <input type="number" ref="age" /></label><br/>
+                    <label>Gender: <input type="radio" ref="value" />Male
+                                   <input type="radio" ref="value" />Female
+                                   <input type="radio" ref="value" />Other
+                    </label><br/>
+                    <label>School: <input type="text" ref="school" /></label><br/>
+                    <label>Job: <input type="text" ref="job" /></label><br/>
 
                     <button type="button" >Sign in</button>
                 </div>
