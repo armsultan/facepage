414faced72a9596a2b5414597e3444695b5cc50b
armsultan <armsultan@gmail.com>
2017-05-31 19:24:59 -0600

    changes for heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 'use strict';
-import env from './env';
+// comment out line below for heroku
+//import env from './env';
 import express from 'express';
 import handlebars from 'express-handlebars';
 import http from 'http';
@@ -15,13 +16,13 @@ const app = express();
 // Setting out app rendering engine to use Handlebars.
 app.engine('.hbs', handlebars({extname: '.hbs'}));
 
-// When express renders a page, it looks in the server/views directory. 
+// When express renders a page, it looks in the server/views directory.
 app.set('views', 'server/views');
 
 // And finally, setting the engine to understand the .hbs extension is to be interpretted by our view engine.
 app.set('view engine', '.hbs');
 
-// here we are telling the express web server to set the static resource folder to 'public'. This is good practice to do so we know where we can put files that will not change. This is where the pictures, CSS, svgs, fonts and more live. 
+// here we are telling the express web server to set the static resource folder to 'public'. This is good practice to do so we know where we can put files that will not change. This is where the pictures, CSS, svgs, fonts and more live.
 app.use(express.static('public'));
 
 // middleware package that allows us to read the body in request objects.
@@ -29,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-// connection string. 
+// connection string.
 mongoose.connect(`mongodb://localhost/${process.env.DB}`);
 
 Routes(app);
